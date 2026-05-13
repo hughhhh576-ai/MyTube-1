8a4a2a83e5c40119ec146f0469a33ae7272b221a
normalcloudvideoplayer-png <normalcloudvideoplayer@gmail.com>
2026-05-13 20:21:06 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-import { registerRootComponent } from 'expo'; import App from './App'; // registerRootComponent calls AppRegistry.registerCompon ent('main', () => App); // It also ensures that whether you load the app in Expo Go or in a native build, // the environment is set up appropriately registerRootComponent (App);
+import { registerRootComponent } from 'expo'; 
+import App from './App'; // registerRootComponent calls AppRegistry.registerCompon ent('main', () => App); // It also ensures that whether you load the app in Expo Go or in a native build, // the environment is set up appropriately registerRootComponent (App);
